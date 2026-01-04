242110315ea30a9fe1a388745ba144a60050a618
Hristo Partenov <hristokubev@gmail.com>
2026-01-04 16:14:17 +0200

    add project structure

---
+++ internal/healthcheck/api_test.go
@@ -0,0 +1,17 @@
+package healthcheck
+
+import (
+	"github.com/ico12319/devops-project/internal/test"
+	"github.com/ico12319/devops-project/pkg/log"
+	"net/http"
+	"testing"
+)
+
+func TestAPI(t *testing.T) {
+	logger, _ := log.NewForTest()
+	router := test.MockRouter(logger)
+	RegisterHandlers(router, "0.9.0")
+	test.Endpoint(t, router, test.APITestCase{
+		"ok", "GET", "/healthcheck", "", nil, http.StatusOK, `"OK 0.9.0"`,
+	})
+}
